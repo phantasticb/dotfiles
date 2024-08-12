ffbf2fc90b9ba1e1e12dddc7afcf10b6ffc83ad0
Byron Phan <phantasticb@gmail.com>
2024-08-11 22:23:45 -0500

    change telescope bindings

--- lua/telescope.lua
+++ lua/telescope.lua
@@ -1,3 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>p', builtin.buffers, {})
